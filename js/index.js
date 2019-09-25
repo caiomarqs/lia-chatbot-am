352ec47bb106ce3125a14ed85a6d71676d5bfcc2
caiomarqs <caiomarques1412@gmail.com>
2019-09-25 13:08:36 -0300

    Inicio Tela Chat

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 // Controlando sombra do menu 
-
 let fun = function() {
     var nav = $('#navbar');
     $(window).scroll(function () {
@@ -10,38 +9,27 @@ let fun = function() {
         }
     });
 };
-
 fun();
 
-let emailInput = document.getElementById('email-modal');
-let emailInput2 = document.getElementById('email-modal2');
-let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-let senhaInput = document.getElementById('senha-modal');
-let senhaRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,16}$/;
+//animação dos botoes do chat
+let btnMic = document.getElementById('btn-mic');
+let btnMsg = document.getElementById('btn-msg');
+let btnMicI = document.getElementById('mic');
+let btnMsgI = document.getElementById('msg');
 
-let inputValidation = (element, regx) =>{
-    var regex = regx;
-    if (element.value.match(regex)) {
-        element.classList.remove("invalid");
-        element.classList.add("valid");
-        return true;
-    } else {
-        element.classList.remove("valid");
-        element.classList.add("invalid");
-        return false;
-    }
-}
-
-
-emailInput.addEventListener('keyup', function() {
-    inputValidation(emailInput, emailRegex);
+btnMic.addEventListener('mouseover' || 'focus', function() {
+    btnMicI.classList.add("btnonhover");
 });
 
-senhaInput.addEventListener('keypress', function() {
-    inputValidation(senhaInput, senhaRegex);
+btnMic.addEventListener('mouseout' || 'blur', function() {
+    btnMicI.classList.remove("btnonhover");
 });
 
-emailInput2.addEventListener('keyup', function() {
-    inputValidation(emailInput2, emailRegex);
+btnMsg.addEventListener('mouseover' || 'focus', function() {
+    btnMsgI.classList.add("btnonhover");
+});
+
+btnMsg.addEventListener('mouseout' || 'blur', function() {
+    btnMsgI.classList.remove("btnonhover");
 });

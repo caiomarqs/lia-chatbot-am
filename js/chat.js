1b19ca45d435d79e6582ea40f1be14f0d8e944e8
caiomarqs <caiomarques1412@gmail.com>
2019-09-26 00:11:19 -0300

    Recebendo msg do input

--- js/chat.js
+++ js/chat.js
@@ -19,3 +19,37 @@ btnMsg.addEventListener('mouseover' || 'focus', function() {
 btnMsg.addEventListener('mouseout' || 'blur', function() {
     btnMsgI.classList.remove("btnonhover");
 });
+
+//receber msg do usuario
+let sysTime = new Date();
+let coproMsg = document.getElementById('corpo-msg');
+let inputusuario = document.getElementById('input-user');
+
+function inserirMsg() {
+    if (inputusuario.value == undefined || inputusuario.value == "") {
+        alert('Digite alguma msg para enviar');        
+    }
+    else{
+        console.log("foi");
+        
+
+        let txtUsr = `<p>${inputusuario.value}</p>`;
+        let dataHora = `<h6 class="data_hora">${sysTime.getHours+""}</h6>`;
+        let boxMsg = `<div class="msg_enviada d-flex flex-column" hidden><h5 class="nome_enviado">Nome</h5><div class="box_msg_enviada">${txtUsr}</div></div>`;
+        let boxMsgElement = new DOMParser().parseFromString(boxMsg, "text/html");
+        coproMsg.appendChild(boxMsgElement.documentElement);
+
+        inputusuario.value = "";
+    }
+}
+
+inputusuario.addEventListener('keypress', function(e) {
+    if (e.which == 13) {
+        inserirMsg();   
+    }
+});
+
+btnMsg.addEventListener('click', function(e) {
+    e.preventDefault();
+    inserirMsg();
+});
